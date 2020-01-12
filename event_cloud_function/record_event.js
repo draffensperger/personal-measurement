e2bb8817e9fa448a66b26b399dc27db9fba322bb
Dave Raffensperger <d.raffensperger@gmail.com>
2020-01-12 15:53:26 -0500

    Add more comments

--- event_cloud_function/record_event.js
+++ event_cloud_function/record_event.js
@@ -7,6 +7,19 @@
  *   SPREADSHEET_ID - ID from `docs.google.com/spreadsheets/d/[ID]/edit` URL
  *   RANGE - spreadsheet range to write event rows, e.g. "Webhook Events!A:B"
  *   KEY - any random secret key used to authenticate clients for writing events
+ *
+ * Note that you will need to given edit access in your Google sheet to the GCP
+ * Service Account that runs your Cloud Function.
+ *
+ * This can be set up with an HTTP trigger for Cloud Functions and then
+ * allows writing events with JSON in the format of:
+ *   {"key":"[YOUR_KEY]","event":"[YOUR_EVENT_NAME"}
+ *
+ * This causes the event name and timestamp to be written to your Google sheet.
+ * This can be used with apps like iOS Shortcuts, IFTTT, or even `curl` commands
+ * in arbitrary scripts (such as OpenWRT device usage tracking) to then enable
+ * various forms of personal habit tracking, such as measuring how often you go
+ * to the gym or how often you use your work computer from your home network.
  */
 
 const {google} = require('googleapis');
